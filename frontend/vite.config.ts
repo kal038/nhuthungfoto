ffb6644cd5070dc5cf7dc9e0d8b445eb3742d256
kal038 <khoialam00@gmail.com>
2026-03-13 22:46:46 +0700

    convert frontend from submodule to dir

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: [
+      "localhost",
+      ".ngrok-free.dev"
+    ]
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
