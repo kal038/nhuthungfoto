bb39ed38391d1ce1404d78dd0cea062f7bb7d40b
kal038 <khoialam00@gmail.com>
2026-04-22 14:55:26 +0700

    chore: track opencode config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,6 +26,7 @@ app.use('*', (c, next) => {
 // Routes Public
 // ---------------------
 app.route('/health', healthRouter)
+
 // ---------------------
 // Routes Protected
 // ---------------------
