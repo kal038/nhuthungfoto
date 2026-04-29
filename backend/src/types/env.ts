d4e2d32111415b3b9386d7a375e5d286a0d65e81
kal038 <khoialam00@gmail.com>
2026-04-29 14:31:42 +0700

    service client w CF worker

--- backend/src/types/env.ts
+++ backend/src/types/env.ts
@@ -35,7 +35,7 @@ export interface Env {
   // ---------------------
   SUPABASE_URL: string
   SUPABASE_ANON_KEY: string
-  SUPABASE_SERVICE_ROLE_KEY: string
+  SUPABASE_SECRET_KEY: string
 
   // ---------------------
   // Config vars
